0b614920dd6fb3bd8f59dd68f7b8f3544b4a5062
Georgi Dinov <georgidinov1@abv.bg>
2021-05-06 17:38:24 +0300

    Added matches page.

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,9 +1,32 @@
+import {useEffect, useState} from "react";
+import {MatchDetailCard} from "../components/MatchDetailCard";
+import {useParams} from "react-router-dom";
+
 export const MatchPage = () => {
+
+    const [matches, setMatches] = useState([]);
+    const {teamName, year} = useParams();
+
+    useEffect(
+        () => {
+            const fetchMatches = async () => {
+                const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+                const data = await response.json();
+                console.log(data);
+                setMatches(data);
+            };
+            fetchMatches();
+
+        }, []
+    );
 
 
     return (
         <div className="MatchPage">
             <h1>Match Page</h1>
+            {
+                matches.map((match => <MatchDetailCard key={match.id} teamName={teamName} match={match}/>))
+            }
         </div>
     );
 }
